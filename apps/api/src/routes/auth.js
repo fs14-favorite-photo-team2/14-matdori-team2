5c77c3b7161b360a5c92e06a56814ef4c0887e3f
taewon-wn <taewon.wn@gmail.com>
2026-09-07 09:50:03 +0900

    feat: 세션 쿠키 및 로그인 로그아웃 구현 (#58)
    - POST /auth/login, POST /auth/logout 엔드포인트 추가
    - 회원가입 시 세션을 생성해 가입 직후 로그인 상태 유지
    - 로그인 시 세션 ID를 재생성하여 세션 고정 공격 방지
    - /auth/logout을 보호하는 requireAuthentication 미들웨어 추가
    - 세션 쿠키 옵션을 분리해 로그아웃 시 동일한 쿠키를 제거

--- apps/api/src/routes/auth.js
+++ apps/api/src/routes/auth.js
@@ -1,11 +1,18 @@
 import { Router } from 'express'
 
-import { signupController } from '../controllers/auth-controller.js'
+import {
+  loginController,
+  logoutController,
+  signupController,
+} from '../controllers/auth-controller.js'
+import { requireAuthentication } from '../middlewares/require-authentication.js'
 import { validateRequest } from '../middlewares/validate-request.js'
-import { signupRequest } from '../validators/auth-validator.js'
+import { loginRequest, signupRequest } from '../validators/auth-validator.js'
 
 const authRouter = Router()
 
 authRouter.post('/signup', validateRequest(signupRequest), signupController)
+authRouter.post('/login', validateRequest(loginRequest), loginController)
+authRouter.post('/logout', requireAuthentication, logoutController)
 
 export default authRouter
