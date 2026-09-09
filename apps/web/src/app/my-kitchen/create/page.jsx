ae2d97c1e9d47bdb9954c596c392221e908012db
wls <jin042078@hs.ac.kr>
2026-09-09 18:45:51 +0900

    fix: 총 발행량 숫자만 입력 가능하도록 수정

--- apps/web/src/app/my-kitchen/create/page.jsx
+++ apps/web/src/app/my-kitchen/create/page.jsx
@@ -212,14 +212,13 @@ export default function CreateRecipePage() {
           </label>
           <input
             id="totalSupply"
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]*"
+            type="number"
+            min="1"
+            max={MAX_SUPPLY}
             className={`${styles.input} ${totalSupply !== '' && !isSupplyValid ? styles.inputError : ''}`}
             value={totalSupply}
             onChange={(e) => {
-              const onlyNums = e.target.value.replace(/[^0-9]/g, '')
-              setTotalSupply(onlyNums)
+              setTotalSupply(e.target.value)
             }}
             placeholder="총 발행량을 입력해 주세요"
           />
